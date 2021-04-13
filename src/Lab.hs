beb2ca57984ee5ccd5ed9b07364d6d0eb23c32fd
Michael Gale <m.gale@warwick.ac.uk>
2021-04-13 12:01:23 +0100

    Add solutions

--- src/Lab.hs
+++ src/Lab.hs
@@ -10,7 +10,15 @@ module Lab where
 
 -- | The infinite list of ones.
 ones :: [Int]
-ones = undefined
+ones = 1 : ones
+
+ones' :: [Int]
+ones' = enumFromThen 1 1
+
+fibs :: [Integer]
+fibs = 1 : xs
+    where xs = 1 : ys
+          ys = zipWith (+) fibs xs
 
 -- | An infinite list modelled after the Fibonacci numbers, but where the
 -- seed values are x and y. (Exponential time complexity!)
@@ -20,6 +28,15 @@ foos x y = x : y : zipWith (+) (foos x y) (tail (foos x y))
 -- | An infinite list modelled after the Fibonacci numbers, but where the
 -- seed values are x and y. (Linear time complexity if you do it right!)
 foos' :: Integer -> Integer -> [Integer]
-foos' x y = undefined
+foos' x y = bar
+    where bar = x : y : zipWith (+) bar (tail bar)
+
+-- void* foos'(int* x, int* y) {
+--      void* bar = malloc(..);
+--      bar[0] = x;
+--      bar[1] = y;
+--      bar[..] 
+--      return bar;
+-- }
 
 --------------------------------------------------------------------------------
